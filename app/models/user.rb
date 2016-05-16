a9ded6bc963ec2752c066ce03549bf5d64b68044
MandisaT <mandisa.thomas1@gmail.com>
2016-05-16 00:21:08 -0400

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+	has_attached_file :profile_pic, :styles =>
+{ :medium => "300x300>", :thumb => "100x100>" },
+:default_url => "/images/:style/missing.png"
+validates_attachment_content_type :profile_pic,
+:content_type => /\Aimage\/.*\Z/
+
+has_secure_password
+validates_presence_of :password
+end
